c1f3fb18deca217f89151902a85f10ccdc70eed1
Abdeljalil <elmarradi@hotmail.fr>
2023-12-28 15:58:52 +0100

    add display accounts with pagination

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AccountsService} from "../services/accounts.service";
+import {Observable} from "rxjs";
+import {AccountDetails} from "../model/account.model";
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.css']
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit{
 
+  accountFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>;
+
+  constructor(private formBuilder : FormBuilder,private accountService : AccountsService) {
+  }
+
+  ngOnInit(): void {
+    this.accountFormGroup = this.formBuilder.group({
+      accountId : this.formBuilder.control("")
+    });
+  }
+
+
+  handleSearchAccount() {
+
+    let accountId : string = this.accountFormGroup.value.accountId;
+    console.log(accountId);
+    this.accountObservable = this.accountService.getAccount(accountId,this.currentPage,this.pageSize);
+  }
+
+  gotoPage( page : number) {
+    this.currentPage = page;
+    this.handleSearchAccount();
+  }
 }
